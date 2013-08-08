322adad6b33637e069c54053b3318639f05b9f11
grasGendarme <ldninten@yahoo.fr>
2013-08-08 12:31:30 +0200

    Adding worldwide best score request

--- model.js
+++ model.js
@@ -15,12 +15,6 @@ redisClient.on('error', function (err) {
 });
 
 
-// looks like 'score::pokemon::bob::'
-function create_redis_key_prefix(game, player) {
-  return ['score', game, player, ''].join('::');
-}
-
-
 // save to redis
 function save_new_score(game, player, score, save_callback) {  
   // set latest, get best saved and replace it by new score if it's is better
@@ -81,37 +75,15 @@ function PUT_handler(req, res) {
 }
 
 
-function GET_handler(req, res) {
-  console.log('Received GET request to url');
-  console.log(req.url);
-  var path = req.url.split('/', 4);
-  
-  // check if input is sane
-  var error_message = '';
-  if (path.length < 4) {
-    error_message = 'Not enough parameters';
-  } else if (path[0] != '') {
-    error_message = 'Invalid request';
-  } else if (config.allowed_game_names.indexOf(path[1]) == -1) {
-    error_message = 'Inexistent game name';
-  } else if (!path[2].match(config.regex_allowed_player_names)) {
-    error_message = 'Invalid player name';
-  } else if (path[3] !== BEST && path[3] !== LATEST) {
-    error_message = 'Invalid [best|latest] keyword';
-  }
-  
-  if (error_message) {
-    console.log('Error: ' + error_message);
-    res.writeHead(400);
-    res.end(error_message);
-    return;
-  }
-  
-  var game = path[1];
-  var player = path[2];
-  var verb = path[3] === BEST ? BEST : LATEST;
-  var key = create_redis_key_prefix(game, player) + verb;
+function presentError(res, error_message) {
+  console.log('Error: ' + error_message);
+  res.writeHead(400);
+  res.end(error_message);
+}
 
+function servePlayerScore(res, game, player, verb) {
+  var key = ['score', game, player, verb].join('::');
+  
   redisClient.get(key, function(err, reply) {
     if (err || isNaN(reply)) {
       res.writeHead(500);
@@ -124,6 +96,76 @@ function GET_handler(req, res) {
       res.end('Player not found');
     }
   });
+}
+
+
+function serveWorldwideBest(res, game) {
+  var key = ['score', game, BEST_EVER].join('::');
+  redisClient.get(key, function(err, reply) {
+    if (err) {
+      res.writeHead(500);
+      res.end('Internal server error');
+    } else if (reply || !isNaN(reply)) {
+      res.writeHead(200);
+      res.end(reply);
+    } else {
+      res.writeHead(404)
+      res.end('Score not found');
+    }
+  });
+}
+
+
+String.prototype.removeTrailing = function(what) {
+  if(this.slice(-what.length) === what) {
+    return this.slice(0, -what.length);
+  } else {
+    return this;
+  }
+}
+
+
+function GET_handler(req, res) {
+  console.log('Received GET request to url');
+  console.log(req.url);
+  var path = req.url.removeTrailing('/').split('/', 4);
+
+  /**
+   * error check for either 
+   * /game/worldwide
+   * or
+   * /game/player/[best|latest]
+   */
+  var error_message = '';
+  if (path.length == 4 || path.length == 3) {
+    // common checks
+    if (path[0] != '') {
+      return presentError(res, 'Invalid request');
+    }
+    if (config.allowed_game_names.indexOf(path[1]) == -1) {
+      return presentError(res, 'Inexistent game name');
+    }
+    if(path.length == 3) {
+      // checks for worldwide
+      if(path[2] === 'worldwide') {
+        return serveWorldwideBest(res, path[1]);
+      }
+      
+      
+    } else {
+      // checks for single player
+      if (!path[2].match(config.regex_allowed_player_names)) {
+        return presentError(res, 'Invalid player name');
+      }
+      if (path[3] !== BEST && path[3] !== LATEST) {
+        return presentError(res, 'Invalid [best|latest] keyword');
+      }
+      // All done, serve the data
+      return servePlayerScore(res, path[1], path[2], path[3]);      
+    }
+  } else {
+    return presentError(res, 'Invalid number of parameters');
+  }
 }
 
 
