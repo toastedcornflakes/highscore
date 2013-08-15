fab50f15cec88c3bb77bc96a702313b113ab0db6
grasGendarme <ldninten@yahoo.fr>
2013-08-15 19:57:47 +0200

    Automatic tests and JSHint with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      // define the files to lint
+      files: ['gruntfile.js', 'server.js', 'model.js', 'model/**.js', 'tests/*.js'],
+      options: grunt.file.readJSON('.jshintrc')
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  // default task launched by 'grunt' on the cmd
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+};
