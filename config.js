6f5b2c8bd77b3d37373cf596849abdc47a20308f
grasGendarme <ldninten@yahoo.fr>
2013-08-18 01:04:39 +0200

    Remove dumb config check

--- config.js
+++ config.js
@@ -8,11 +8,6 @@ var config = {};
 config.allowed_game_names = ['pokemon' , 'zelda'];
 
 config.allowed_player_names = /^[a-z0-9]{2,32}$/i;
-
-// check there's only alphanumeric in game_names
-config.allowed_game_names.forEach(function(element){
-  assert(element.match(/^[a-z0-9]+$/i), 'Invalid game name in config');
-});
 assert(_.isRegExp(config.allowed_player_names), 'Allowed player name should be a RegExp');
 
 module.exports = config;
