9cfa241ac0d55904c9aaa4c1e9d0c92603bf6480
grasGendarme <ldninten@yahoo.fr>
2013-08-18 01:03:37 +0200

    Limit username size

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = {};
 
 config.allowed_game_names = ['pokemon' , 'zelda'];
 
-config.allowed_player_names = /^[a-z0-9]+$/i;
+config.allowed_player_names = /^[a-z0-9]{2,32}$/i;
 
 // check there's only alphanumeric in game_names
 config.allowed_game_names.forEach(function(element){
